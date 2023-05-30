89d918c28e02a235c2afca3a628dcebb5f544fd0
Nikolaos Takos <takosnikolaos@gmail.com>
2023-05-30 13:10:19 +0300

    Added the basic functions of the calculator.

--- app.js
+++ app.js
@@ -1,2 +1,43 @@
-let x, y;
+let x, y, res;
+
+document.getElementById("+").onclick = function(){
+    x = document.getElementById("xText").value;
+    y = document.getElementById("yText").value;
+    x = Number(x);
+    y = Number(y);
+    res = x + y;
+    console.log(res)
+    document.getElementById("final").innerHTML = res;
+}
+
+document.getElementById("-").onclick = function(){
+    x = document.getElementById("xText").value;
+    y = document.getElementById("yText").value;
+    x = Number(x);
+    y = Number(y);
+    res = x - y;
+    console.log(res)
+    document.getElementById("final").innerHTML = res;
+}
+
+document.getElementById("*").onclick = function(){
+    x = document.getElementById("xText").value;
+    y = document.getElementById("yText").value;
+    x = Number(x);
+    y = Number(y);
+    res = x * y;
+    console.log(res)
+    document.getElementById("final").innerHTML = res;
+}
+
+document.getElementById("/").onclick = function(){
+    x = document.getElementById("xText").value;
+    y = document.getElementById("yText").value;
+    x = Number(x);
+    y = Number(y);
+    res = x / y;
+    console.log(res)
+    document.getElementById("final").innerHTML = res;
+}
+
 
