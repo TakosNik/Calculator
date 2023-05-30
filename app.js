35acbcf12ef496f217299ef07dc6196a12f9344d
Nikolaos Takos <takosnikolaos@gmail.com>
2023-05-30 15:25:34 +0300

    Added x^y calculation button, finished styles.

--- app.js
+++ app.js
@@ -40,6 +40,17 @@ document.getElementById("/").onclick = function(){
     document.getElementById("final").innerHTML = res;
 }
 
+
+document.getElementById("power").onclick = function(){
+    x = document.getElementById("xText").value;
+    y = document.getElementById("yText").value;
+    x = Number(x);
+    y = Number(y);
+    res = Math.pow(x, y);
+    console.log(res);
+    document.getElementById("final").innerHTML = res;
+}
+
 document.getElementById("clearform").onclick = function(){
     res = " ";
     document.getElementById("final").innerHTML = res;
